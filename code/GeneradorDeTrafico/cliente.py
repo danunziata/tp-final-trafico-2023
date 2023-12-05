d3a73b85759e719a1a46007dbb0532719c41f81c
andresbuten2002 <butenandres@gmail.com>
2023-12-05 15:29:34 -0300

    fix: Eliminamos archivo cliente.py repetido

--- code/GeneradorDeTrafico/cliente.py
+++
@@ -1,9 +0,0 @@
-from locust import HttpUser, task, between
-import random, asyncio
-#lambd=1000
-class HelloWorldUser(HttpUser):
-
-    @task
-    def hello_world(self):
-        asyncio.sleep(0.01)
-        self.client.get("/")

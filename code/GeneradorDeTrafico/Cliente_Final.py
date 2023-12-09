e8d86cef28bc50f3a0234df5b0f5f31a682e2a2c
andresbuten2002 <butenandres@gmail.com>
2023-12-09 10:18:47 -0300

    feat: Añadimos Cliente_Final.py

---
+++ code/GeneradorDeTrafico/Cliente_Final.py
@@ -0,0 +1,59 @@
+import aiohttp
+import asyncio
+import random
+
+# Lista para almacenar los tiempos de respuesta
+response_times = []
+
+async def send_request(session, host, port, path, user_id):
+    url = f"http://{host}:{port}{path}"
+
+    # Medir el tiempo antes de enviar la solicitud
+    start_time = asyncio.get_event_loop().time()
+
+    # Iniciar la solicitud sin esperar la respuesta
+    async with session.get(url) as response:
+        #Para hacer que no espere la respuesta del servidor quitar el comentario en la siguiente linea y comentar la otra seccion
+        #-----pass
+
+        #La siguientes lineas hacen que el cliente espere la respuesta 
+        data = await response.text()
+        #-----print(f"Response from server: {data}")
+       
+
+    # Medir el tiempo después de recibir la respuesta y almacenar el tiempo de respuesta
+    end_time = asyncio.get_event_loop().time()
+    response_time = end_time - start_time
+    response_times.append(response_time)
+    print(f"response time  {response_time}")
+
+    # Mostrar el tiempo promedio cada 100 paquetes
+    if len(response_times) % 100 == 0:
+        average_response_time = sum(response_times) / len(response_times)
+        print(f"Avg. response time after {len(response_times)} packets: {average_response_time} seconds")
+
+async def generate_traffic(user_id, session, host, port, path, lambda_value):
+    while True:
+        # Esperar un tiempo según la distribución exponencial antes de comenzar
+        inter_arrival_time = random.expovariate(lambda_value)
+        #-----print(f"User {user_id}: Will wait for {inter_arrival_time} seconds before starting")
+
+        await asyncio.sleep(inter_arrival_time)
+
+        # Ejecutar la solicitud
+        await send_request(session, host, port, path, user_id)
+
+async def main():
+    host = "192.168.1.109"
+    port = 2023  # Reemplaza con el puerto correcto
+    path = "/" # Reemplaza con la ruta correcta
+
+    num_users = int(input("Ingrese la cantidad de usuarios: "))
+    lambda_value = float(input("Ingrese el valor de lambda para la distribución exponencial: "))
+
+    async with aiohttp.ClientSession() as session:
+        user_tasks = [generate_traffic(user_id, session, host, port, path, lambda_value) for user_id in range(1, num_users + 1)]
+        await asyncio.gather(*user_tasks)
+
+if __name__ == "__main__":
+    asyncio.run(main())
